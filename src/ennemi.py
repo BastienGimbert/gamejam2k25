41cde120532d1ed76268d3c9c091c4010b59c902
feryal0903 <feryal0903@gmail.com>
2025-09-03 11:33:26 +0200

    ennemi class

---
+++ src/ennemi.py
@@ -0,0 +1,14 @@
+class Ennemi:
+    def __init__(self, vitesse, pointDeVie, position, degat):
+        self.vitesse = vitesse
+        self.pointDeVie = pointDeVie
+        self.position = position
+        self.degat =degat
+
+    def apparaitre():
+
+    def seDeplacer():
+
+    def perdreVie(degat : int):
+
+    def getDistance():        
