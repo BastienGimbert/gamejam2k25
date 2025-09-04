0e57dd3b8ebb2956eeb60889899d52e79ae00ae8
gibellog <gregoire38600@gmail.com>
2025-09-04 17:26:09 +0200

    augmentation des degats des ennemis

--- src/classes/ennemi.py
+++ src/classes/ennemi.py
@@ -133,7 +133,7 @@ class Gobelin(Ennemi):
         return "Gobelin"
 
     def __init__(self, tempsApparition: int, chemin: Optional[List[Position]] = None, **kw):
-        super().__init__(tempsApparition=tempsApparition, vitesse=50.0, pointsDeVie=70, degats=1, chemin=chemin, **kw)
+        super().__init__(tempsApparition=tempsApparition, vitesse=50.0, pointsDeVie=70, degats=10, chemin=chemin, **kw)
 
         # Charger les spritesheets une seule fois
         if Gobelin._frames_by_dir is None:
@@ -184,7 +184,7 @@ class Rat(Ennemi):
     def type_nom(self) -> str: return "Rat"
 
     def __init__(self, tempsApparition: int, chemin: Optional[List[Position]] = None, **kw):
-        super().__init__(tempsApparition=tempsApparition, vitesse=120.0, pointsDeVie=20, degats=1, chemin=chemin, **kw)
+        super().__init__(tempsApparition=tempsApparition, vitesse=120.0, pointsDeVie=20, degats=4, chemin=chemin, **kw)
 
         if Rat._frames_by_dir is None:
             Rat._frames_by_dir = {
@@ -227,7 +227,7 @@ class Loup(Ennemi):
     def type_nom(self) -> str: return "Loup"
 
     def __init__(self, tempsApparition: int, chemin: Optional[List[Position]] = None, **kw):
-        super().__init__(tempsApparition=tempsApparition, vitesse=100.0, pointsDeVie=90, degats=2, chemin=chemin, **kw)
+        super().__init__(tempsApparition=tempsApparition, vitesse=100.0, pointsDeVie=90, degats=10, chemin=chemin, **kw)
 
         if Loup._frames_by_dir is None:
             Loup._frames_by_dir = {
@@ -270,7 +270,7 @@ class Mage(Ennemi):
     def type_nom(self) -> str: return "Mage"
 
     def __init__(self, tempsApparition: int, chemin: Optional[List[Position]] = None, **kw):
-        super().__init__(tempsApparition=tempsApparition, vitesse=70.0, pointsDeVie=180, degats=3, chemin=chemin, **kw)
+        super().__init__(tempsApparition=tempsApparition, vitesse=70.0, pointsDeVie=180, degats=15, chemin=chemin, **kw)
 
         if Mage._frames_by_dir is None:
             Mage._frames_by_dir = {
